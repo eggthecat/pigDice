4dab7aab25fc6f94c71d2e3fe7dd2d0d04451d03
dominic montelongo and Duncan Robbins <dominic@>
2019-08-28 15:48:35 -0700

    Add business logic for creating an array of players and attaching properties to them

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,87 @@
 // business logic
-var currentPlayer = 0;
-
-function player (startScore, startTurn) {
-  this.score = startScore;
-  this.turn = startTurn;
-  this.turnScore = startTurnScore;
-
-  if (score >= 100) {
-    alert("winner");
-  };
+// function for establishing array of players
+function playerRoster() {
+  this.players = [""];
+  this.activePlayer = 0;
 };
 
-player.prototype.roll = function () {
+// prototype for adding a player
+playerRoster.prototype.addPlayer = function(player) {
+  this.players.push(player);
+};
+// constructor for player
+function player(name, playerId) {
+  this.name = name;
+  this.playerID = newIDforPlayer +=1;
+  this.roll = 0,
+  this.score = 0,
+};
 
-}
 
 
+// var diceRoll = Math.floor( Math.random() * 6 ) +1;
+//
+// var currentPlayer = 0;
+
+
+//  player (startScore) {
+//   this.score = startScore;
+// };
+
+
+// player.prototype.roll = function (startScore) {
+//   function roll (min, max) {
+//     return Math.random() * (max-min)+ min;
+//     console.log(roll);
+
+// press roll for random number
+  //get random number in range
+    // start if/else
+    // if (roll ==! 1) {
+    //if number is 1
+      //lose all their turn score
+        //swap players
+
+    //if number is not > 1
+      //add number to turn playerOneTurnScore/playerTwoTurnScore
+
+    // press hold
+      //store the random number
+        // push random number into array for player score
+          // adjust overall score
+
+    // change player
+      // repeat process of rolling dice
+
+
+  // reset button
+    // all scores are turned to 0
+      // player 1 begins
+
+
+
+  //   };
+  // };
 
 // user interface
 $(document).ready(function(){
-  $("button#start").click(function(event){
+  $("button#restart").click(function(event){
     event.preventDefault();
-    var start = $("#startButton").val();
-    var reset = $("#resetButton").val();
-    var hold = $("#holdButton").val();
+    // var start = $("#startButton").val();
+    var restart = $("#restartButton").val();
     var roll = $("#rollButton").val();
-  })
+    var hold = $("#holdButton").val();
 
+  });
 });
+
+
+
+
+
+
+
+function restart(){
+  $(".playerScore").val(0);
+  $(".turnScore").val(0);
+}
